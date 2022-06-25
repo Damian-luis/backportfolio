2621b1f993373f12580b2332fc7f665fd1ac678b
Damian-luis <damian.luis.porta@gmail.com>
2022-06-25 00:36:55 -0300

    done

--- index.js
+++ index.js
@@ -23,69 +23,113 @@ app.get('/informacion', function(req, res){
     res.send("api funcionando correctamente")
 })
 
-/*
+
+
 app.post('/enviar',async (req, res)=>{
-console.log(req.body)
-const name = req.body.name
-const number = req.body.number
-const company = req.body.company
-const message = req.body.message
-const email = req.body.mail
-const file = req.body.file
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-      // true for 465, false for other ports
-      auth: {
-        user: 'damian.duran@webleadsgroup.com', // generated ethereal user
-        pass: 'jdlodywdllwkvzjg', // generated ethereal password
-      }
-    
-    });
-
-
-
-    var mailOptions = {
-      from: name,
-      to: "damian.luis.porta@gmail.com", // list of receivers
-      subject: "Nuevo mensaje en Webleads!", // Subject line
-       // plain text body
-      text: "",
-      html: `<div style="width:100%;height:auto; box-sizing: border-box;background-color:black;border-radius:15px;">
-
-      <div style="justify-content:center;width:100%;height:10%;background: rgb(18,131,215);
-      background: linear-gradient(90deg, rgba(18,131,215,1) 0%, rgba(204,21,58,1) 49%, rgba(232,215,18,1) 100%);">
-      <h3 style="color:white;text-align: center;">Webleads Group </h3>
-      </div>
-
-      <div style="padding:20px">
-      <div style="background-color: rgba(255, 255, 255, 0.37); height:80%;width:90%;border:solid 1px white;border-radius:10px;padding:10px;color:white;">
-      <h4>Mensaje recibido de: ${name} <br>
-          Compañía: ${company} <br>
-          Numero de contacto: ${number} <br>
-          Correo electrónico: ${email} <br>
-          Mensaje: <br><br>
-      
-      <h2 style="text-align:center;">${message}</h1>
-      </div> </div>
-      
-      </div>`,// html body
-      attachments: file
-    }
-    
-    
-    transporter.sendMail(mailOptions,(error,info) => {
-      console.log("senMail returned!");
-      if (error) {
-        console.log("ERROR!!!!!!", error);
-      } else {
-        console.log('Email sent: ' + info.response);
-      }
-    });
+  console.log(req.body)
+  const name = req.body.name
+  const message = req.body.message
+  const mail= req.body.mail
   
-return res.send("Mensaje enviado exitosamente a webleads! ")
-
-   
-})  */
+  
+  function mailToMyself(){
+      
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+        // true for 465, false for other ports
+        auth: {
+          user: 'damian.luis.porta@gmail.com', // generated ethereal user
+          pass: 'bhfpebetrkzfinzu', // generated ethereal password
+        }
+      
+      });
+  
+  
+  
+      var mailOptions = {
+        from: '"Portfolio" <damian.luis.porta@gmail.com>', // sender address
+        to: `damian.luis.porta@gmail.com`, // sender addresslist of receivers
+        subject: "Nuevo Mensaje desde tu potfolio!", // Subject line
+         // plain text body
+        html: `<h4>Nombre del cliente: ${name} <br>
+        Mail de contacto: ${mail}<br>
+        Mensaje:</h4><br><br><br>
+        <h3> ${message}</h3>`,
+        // html bod
+        
+      }
+      
+      
+      transporter.sendMail(mailOptions,(error,info) => {
+        console.log("senMail returned!");
+        if (error) {
+          console.log("ERROR!!!!!!", error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+      
+  }
+  
+  
+  function mailAutoReply(){
+      
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+        // true for 465, false for other ports
+        auth: {
+          user: 'damian.luis.porta@gmail.com', // generated ethereal user
+          pass: 'bhfpebetrkzfinzu', // generated ethereal password
+        }
+      
+      });
+  
+  
+  
+      var mailOptions = {
+        from: '"Damian Duran" <damian.luis.porta@gmail.com>', // sender address
+        to: `${mail}`, // list of receivers
+        subject: "Damian Duran", // Subject line
+         // plain text body
+         text: "",
+        html: `<div style="height:auto;width:100vw;border:solid 1px black;padding:15px;border-radius:10px;">
+        
+        <h2>Muchas gracias por comunicarte conmigo ☺! <h2><br>
+        <h2>He recibido tu mensaje exitosamente, me pondre en contacto con vos muy pronto, muchas gracias! :) .</h2>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <h3>Damian Duran. </h3>
+        
+        
+        </div>`// html body
+      }
+      
+      
+      transporter.sendMail(mailOptions,(error,info) => {
+        console.log("senMail returned!");
+        if (error) {
+          console.log("ERROR!!!!!!", error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+      
+  }
+  
+  var mails = [mailToMyself,mailAutoReply]
+  
+  
+  for (var i=0; i<2;i++){
+      mails[i]()
+  }
+    
+  return res.send("mensaje enviado correctamente")
+     
+  })
 
 
 
